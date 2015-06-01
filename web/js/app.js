87f0c66b8f2ce086d48078c4646ea4a76c17b5e5
paul <paul@gamillc.com>
2015-06-01 21:41:48 +0530

    image gallery

--- web/js/app.js
+++ web/js/app.js
@@ -5,8 +5,8 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: GenericViewCtrl});
-    $routeProvider.when('/shop', {templateUrl: 'partials/shop.html', controller: GenericViewCtrl});
+    $routeProvider.when('/shop', {templateUrl: 'partials/shop.html', controller: shopCtrl});
     $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: GenericViewCtrl});
-    $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactViewCtrl});
+    $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: GenericViewCtrl});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
--- web/js/controllers.js
+++ web/js/controllers.js
@@ -2,7 +2,6 @@
 
 /* Controllers */
 
-
 function GenericViewCtrl($scope) {
 }
 GenericViewCtrl.$inject = ['$scope'];
@@ -12,37 +11,115 @@ function ContactViewCtrl($scope, $http) {
 
     $scope.lastForm = {};
 
-    $scope.sendForm = function(form) {
-        $scope.lastForm = angular.copy(form);
-        $http({
-            method: 'POST',
-            url: "/backend/email.php",
-            data: {
-                'contactname':$scope.form.name,
-                'weburl':$scope.form.website,
-                'email':$scope.form.email,
-                'app':$scope.form.project,
-                'subject':$scope.form.subject,
-                'message':$scope.form.message
-            },
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-        }).success(function(data, status, headers, config) {
-                $scope.resultData = data;
-                alert("Message sent successfully. We'll get in touch with you soon.");
-
-            }).error(function(data, status, headers, config) {
-                $scope.resultData = data;
-                alert("Sending message failed.");
-            });
-    }
-
     $scope.resetForm = function() {
         $scope.form = angular.copy($scope.lastForm);
     }
 
 }
 
+function shopCtrl($scope, $http, $rootScope ) {
+
+  $rootScope.SelectedImg = '10.jpg';
+  $rootScope.leftImgId = 0;
+  $rootScope.bottomImgId = 0;
+  $scope.lastForm = {};
+
+  $scope.leftImages = [
+    {'id': '1',
+     'img': '1.jpg'},
+    {'id': '2',
+     'img': '2.jpg'},
+    {'id': '3',
+     'img': '3.jpg'},
+    {'id': '4',
+     'img': '4.jpg'}
+  ];
+
+  $scope.bottomImages = [
+    {'id': '1',
+     'img': '1.jpg'},
+    {'id': '2',
+     'img': '2.jpg'},
+  ];
+
+  $scope.displayImages = [
+    {'leftImgId': '1','BottomImgIid': '1','img': '1.jpg'},
+    {'leftImgId': '1','BottomImgIid': '2','img': '2.jpg'},
+    {'leftImgId': '2','BottomImgIid': '1','img': '3.jpg'},
+    {'leftImgId': '2','BottomImgIid': '2','img': '4.jpg'},
+    {'leftImgId': '3','BottomImgIid': '1','img': '5.jpg'},
+    {'leftImgId': '3','BottomImgIid': '2','img': '6.jpg'},
+    {'leftImgId': '4','BottomImgIid': '1','img': '7.jpg'},
+    {'leftImgId': '4','BottomImgIid': '2','img': '8.jpg'},
+   
+  ];
+
+  $scope.resetForm = function() {
+    
+    $scope.form = angular.copy($scope.lastForm);
+  }
+
+  $scope.prev = function () {
+    
+    alert("prev pressed");
+  }
+
+  $scope.next = function () {
+    
+    alert("next pressed");
+  }
+
+  $scope.getLeftImg = function ( id ) {
+
+    // alert(id);
+    $rootScope.leftImgId = id;
+    $scope.get_image();
+  }
+  
+  $scope.getBottomImg = function ( id ) {
+
+    $rootScope.bottomImgId = id;
+    $scope.get_image();
+
+  }
+
+  $scope.get_image = function() {
+
+    console.log($rootScope.leftImgId + '::' + $rootScope.bottomImgId );
+    // have to cal api to get the right image here
+    // for test: using if condition
+
+    if ( $rootScope.leftImgId == 1 && $rootScope.bottomImgId == 1 ) {
+
+      $rootScope.SelectedImg = '1.jpg';
+    } else if ( $rootScope.leftImgId == 2 && $rootScope.bottomImgId == 1 ) {
+
+      $rootScope.SelectedImg = '2.jpg';
+    } else if ( $rootScope.leftImgId == 3 && $rootScope.bottomImgId == 1 ) {
+
+      $rootScope.SelectedImg = '3.jpg';
+    } else if ( $rootScope.leftImgId == 4 && $rootScope.bottomImgId == 1 ) {
+
+      $rootScope.SelectedImg = '4.jpg';
+    } else if ( $rootScope.leftImgId == 1 && $rootScope.bottomImgId == 2 ) {
+
+      $rootScope.SelectedImg = '5.jpg';
+    } else if ( $rootScope.leftImgId == 2 && $rootScope.bottomImgId == 2 ) {
+
+      $rootScope.SelectedImg = '6.jpg';
+    } else if ( $rootScope.leftImgId == 3 && $rootScope.bottomImgId == 2 ) {
+
+      $rootScope.SelectedImg = '7.jpg';
+    }  else if ( $rootScope.leftImgId == 4 && $rootScope.bottomImgId == 2 ) {
+
+      $rootScope.SelectedImg = '8.jpg';
+    } else
+    {
+      $rootScope.SelectedImg = '10.jpg';
+
+    }
+  };
+
+}
+
 ContactViewCtrl.$inject = ['$scope', '$http'];
-
-
-
